d10fab4cc665f75a706bb8db7e9c7596211a5f06
B0yscout <nicolas.chataignon@protonmail.com>
2022-07-29 19:48:00 +0200

    debug false

--- anomaPro/settings.py
+++ anomaPro/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-e*^o)8bmr5x3xra)j2-ix8*^+emkqqik-84mw8$#$=qn$webh+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.localhost']
 
